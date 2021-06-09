ec26c0da4a744d2519b75c06a2ae724f075ea7bc
YOUGUO XIONG <xiongyouguo@360.cn>
2021-06-09 16:08:02 +0800

    flink-1.12.4 源码例子

--- code/flink-learning/flink-batch/src/main/java/edu/bear/flink/examples/java/wordcount/WordCount.java
+++ code/flink-learning/flink-batch/src/main/java/edu/bear/flink/examples/java/wordcount/WordCount.java
@@ -1,23 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.bear.flink.examples.java.wordcount;
 
+import edu.bear.flink.examples.java.wordcount.util.WordCountData;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.MultipleParameterTool;
+
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.Preconditions;
 
 /**
- * @author Mr.bear
- * @version 1.0.0
- * @className WordCount
- * @description TODO
- * @createTime 2020/11/06 16:18
+ * Implements the "WordCount" program that computes a simple word occurrence histogram over text
+ * files.
+ *
+ * <p>The input is a plain text file with lines separated by newline characters.
+ *
+ * <p>Usage: <code>WordCount --input &lt;path&gt; --output &lt;path&gt;</code><br>
+ * If no parameters are provided, the program is run with default data from {@link WordCountData}.
+ *
+ * <p>This example shows how to:
+ *
+ * <ul>
+ *   <li>write a simple Flink program.
+ *   <li>use Tuple data types.
+ *   <li>write and use user-defined functions.
+ * </ul>
  */
 public class WordCount {
 
-// *************************************************************************
+    // *************************************************************************
     //     PROGRAM
     // *************************************************************************
 
@@ -66,7 +96,6 @@ public class WordCount {
             System.out.println("Printing result to stdout. Use --output to specify output path.");
             counts.print();
         }
-
     }
 
     // *************************************************************************
@@ -75,10 +104,11 @@ public class WordCount {
 
     /**
      * Implements the string tokenizer that splits sentences into words as a user-defined
-     * FlatMapFunction. The function takes a line (String) and splits it into
-     * multiple pairs in the form of "(word,1)" ({@code Tuple2<String, Integer>}).
+     * FlatMapFunction. The function takes a line (String) and splits it into multiple pairs in the
+     * form of "(word,1)" ({@code Tuple2<String, Integer>}).
      */
-    public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+    public static final class Tokenizer
+            implements FlatMapFunction<String, Tuple2<String, Integer>> {
 
         @Override
         public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
